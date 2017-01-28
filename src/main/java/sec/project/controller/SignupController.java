a911b9713c33ec31e6cdb37b0237bf82f2d38992
Aku Silvennoinen <augustus58@gmail.com>
2017-01-28 21:06:21 +0200

    A3-Cross-Site_Scripting flaw and documentation added

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -2,9 +2,12 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
 
@@ -20,19 +23,33 @@ public class SignupController {
   }
 
   @RequestMapping("/done")
-  public String done() {
+  public String done(Model model, @RequestParam String id) {
+    Signup s = signupRepository.findOne(new Long(id));
+    model.addAttribute("name", s.getName());
+    model.addAttribute("address", s.getAddress());
     return "done";
   }
-  
+
   @RequestMapping(value = "/form", method = RequestMethod.GET)
   public String loadForm() {
     return "form";
   }
 
   @RequestMapping(value = "/formSave", method = RequestMethod.GET)
-  public String submitForm(@RequestParam String name, @RequestParam String address, @RequestParam String redirect) {
-    signupRepository.save(new Signup(name, address));
+  public String submitForm(@RequestParam String name, @RequestParam String address, @RequestParam String redirect, RedirectAttributes redirectAttributes) {
+    Signup s = signupRepository.save(new Signup(name, address));
+    redirectAttributes.addAttribute("id", s.getId());
     return (redirect != null && !redirect.isEmpty() ? "redirect:" + redirect : "form");
+  }
+
+  @RequestMapping(value = "/signups", method = RequestMethod.GET)
+  @ResponseBody
+  public String signups(Model model) {
+    String list = "";
+    for (Signup s : signupRepository.findAll()) {
+      list = list.concat(s.getName() + " " + s.getAddress() + "<br>");
+    }
+    return list;
   }
 
 }
